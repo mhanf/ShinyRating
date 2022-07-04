4d1835657930437690720d86f4486c2d52b53f76
Matthieu HANF <matthieu.hanf@sesan.fr>
2022-07-04 21:47:52 +0200

    commit pkgdown

--- inst/app/global.R
+++ inst/app/global.R
@@ -11,18 +11,19 @@ theme <- bslib::bs_theme(
 card_suite <- function(
   title,
   body
-){
-  div(class = "col",
+) {
+  div(
+    class = "col",
+    tags$div(
+      class = "card text-center h-100",
       tags$div(
-        class = "card text-center h-100",
-        tags$div(
-          class = "card-header primary-text fw-bold",
-          title
-          ),
-        tags$div(
-          class = "card-body",
-          body
-        )
+        class = "card-header primary-text fw-bold",
+        title
+      ) ,
+      tags$div(
+        class = "card-body",
+        body
       )
+    )
   )
 }
